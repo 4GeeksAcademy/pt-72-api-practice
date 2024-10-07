ed7eef87e094558e050b99b121411a93958d6fb3
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2024-10-07 22:53:38 +0000

    api and input display demo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,68 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+	const [userInput, setUserInput] = useState("");
+	const [userList, setUserList] = useState([]);
+	const [starships, setStarships] = useState([])
+	const [user, setUser] = useState()
+
+	useEffect(() => {
+		async function getStarships() {
+			let response = await fetch("https://www.swapi.tech/api/starships")
+			let data = await response.json()
+			setStarships(data.results)
+		}
+		getStarships()
+	}, []);
+
+	useEffect(() => {
+		async function checkForuser() {
+			let response = await fetch("https://playground.4geeks.com/todo/users/thisUserName")
+			let data = await response.json()
+			if(data.detail !== "User thisUserName doesn't exist.") {
+				setUser(data);
+			}
+			else {
+				let response = await fetch("https://playground.4geeks.com/todo/users/thisUserName", {
+					method: "POST",
+					headers: { "Content-type": "application/json" },
+				})
+				let data = await response.json()
+				console.log(data)
+			}
+		}
+	}, [])
+
+	const addToList = () => {
+		setUserList([...userList, userInput]);
+		// let newArray = userList.push(userInput);
+		// setUserList(newArray);
+		setUserInput("")
+	};
+	const deleteFromList = (deleted) => {
+		const updatedArray = userList.filter((item, index) => {
+			return item !== deleted
+		})
+		setUserList(updatedArray)
+	};
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input value={userInput} onChange={(ev) => setUserInput(ev.target.value)}/>
+			<button onClick={() => addToList()}>Display Input</button>
+
+			<ol>
+				{userList?.map((item, index) => (
+					<li key={index}>{item}<span onClick={() => deleteFromList(item)}> Delete</span></li>
+				))}
+			</ol>
+
+			<ol>
+				{starships?.map((ship, index) => (
+					<li key={index}>{ship.name}<span onClick={() => deleteFromList(item)}> Delete</span></li>
+				))}
+			</ol>
 		</div>
 	);
 };
